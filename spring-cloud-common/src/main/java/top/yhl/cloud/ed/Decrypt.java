be1b8cc6c17303a8b6ff2a5204ed0ed69c2202a2
jiangxiaokai <541657414@qq.com>
2021-09-24 15:58:51 +0800

    decode

---
+++ spring-cloud-common/src/main/java/top/yhl/cloud/ed/Decrypt.java
@@ -0,0 +1,11 @@
+package top.yhl.cloud.ed;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD, ElementType.PARAMETER})
+public @interface Decrypt {
+}
---
+++ spring-cloud-common/src/main/java/top/yhl/cloud/ed/EncryptAutoConfiguration.java
@@ -0,0 +1,9 @@
+package top.yhl.cloud.ed;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("top.yhl.cloud.ed")
+public class EncryptAutoConfiguration {
+}
