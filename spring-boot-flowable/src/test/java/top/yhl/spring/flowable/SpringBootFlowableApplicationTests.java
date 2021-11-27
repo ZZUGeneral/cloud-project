bdcaedcbda775599fa453dd9eb6f881b990feae8
ZZUGeneral <1359541394@qq.com>
2021-11-27 15:46:02 +0800

    threadpool monitor

---
+++ spring-boot-flowable/src/test/java/top/yhl/spring/flowable/SpringBootFlowableApplicationTests.java
@@ -0,0 +1,13 @@
+package top.yhl.spring.flowable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFlowableApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
